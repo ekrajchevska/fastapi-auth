536966f2334c94d5ceef9ff72b28dd55a2cdec07
ekrajchevska <eva.krajchevska@gmail.com>
2021-10-06 14:56:47 +0200

    dbs secrets added (k8s problems)

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+
+postgres_user = os.getenv("POSTGRES_USER")
+postgres_password = os.getenv("POSTGRES_PASSWORD")
+postgres_host = os.getenv("POSTGRES_HOST")
+postgres_port = os.getenv("POSTGRES_PORT")
+postgres_database = os.getenv("POSTGRES_DATABASE_FASTAPI")
+
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./auth_app.db"
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost:5432/auth_users"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@0.0.0.0:5432/auth_users"
-# SQLALCHEMY_DATABASE_URL = "sqlite:////auth_app.db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@0.0.0.0:5432/auth_users"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}"
 
 # for docker localhost on windows is host.docker.internal
 # if we also run postgres container then with docker-compose the host is service_name
